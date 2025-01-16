be5a7989f6ae03e0360d3104872aa92f3d7759ff
최현정 <hjchoi.dev@gmail.com>
2025-01-17 00:57:18 +0900

    build: Update compileSdk

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 android {
     namespace = "com.hyunjung.jetpackcomposemasterclass"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.hyunjung.jetpackcomposemasterclass"
